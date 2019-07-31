013f9f27c445eecac8b070fa9b42bc8c8c06097d
Goose <46300854+goose555@users.noreply.github.com>
2019-07-31 22:36:11 +0300

    Mutex lock_guard
    Example

---
+++ Mutex_2/Mutex_2/Mutex_2.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <mutex>
+#include <thread>
+#include "SimpleTimer.h"
+
+using namespace std;
+
+mutex mtx;
+
+void Print(char ch) {
+
+	
+	this_thread::sleep_for(chrono::milliseconds(2000));
+	//mtx.lock();
+
+	{//Ограничение guard
+		lock_guard<mutex> guard(mtx);
+		for (int i = 0; i < 5; ++i)
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				cout << ch;
+				this_thread::sleep_for(chrono::milliseconds(20));
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}//Ограничение guard
+
+	this_thread::sleep_for(chrono::milliseconds(2000));
+	//mtx.unlock();
+
+}
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	SimpleTimer timer;
+
+	thread t1(Print, '*');
+	thread t2(Print, '#');
+	thread t3(Print, '@');
+
+	t1.join();
+	t2.join();
+	t3.join();
+
+
+	return 0;
+}
